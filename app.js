b60abf46b12d0db449ece452fcfd22e88e6c2e16
Haroldo Júnio <haroldojunios@gmail.com>
2021-12-07 09:43:46 -0300

    Aula 5
    Criação de listas customizadas

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -22,6 +22,13 @@ const item2 = new Item({ name: "Hit the + button to add a new item." });
 const item3 = new Item({ name: "<-- Hit this to delete an item." });
 
 const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function (req, res) {
   Item.find({}, function (err, foundItems) {
@@ -57,12 +64,29 @@ app.post("/delete", function (req, res) {
       console.log("Successfully deleted checked item.");
     }
   });
-  
+
   res.redirect("/");
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+app.get("/:customListName", function (req, res) {
+  const customListName = req.params.customListName;
+
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        // Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        // Show an existing list
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+      }
+    }
+  });
 });
 
 app.get("/about", function (req, res) {
